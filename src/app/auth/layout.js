3a05fe7744fd05a16d0c699bbe130f8f3ea66f9c
IoanTimis <timisionut2000@gmail.com>
2025-03-26 15:59:06 +0200

    save progress

--- src/app/auth/layout.js
+++ src/app/auth/layout.js
@@ -3,9 +3,12 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import {jwtDecode} from "jwt-decode";
+import { useState } from "react";
+
 
 export default function AuthGuardLayout({ children }) {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
@@ -21,9 +24,15 @@ export default function AuthGuardLayout({ children }) {
         }
       } catch (e) {
         console.error("Invalid token:", e);
+      } finally {
+        setLoading(false);
       }
     }
   }, [router]);
+
+  if (loading) {
+    return <div></div>;
+  }
 
   return <>{children}</>;
 }
